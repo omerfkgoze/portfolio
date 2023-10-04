efb13b19334ff7dc560e25d352d1ef038c1602ba
omerfkgoze <farukucukgoze@gmail.com>
2023-10-04 23:58:06 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">About Me</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Hello there! I&apos;m a Junior Full Stack Developer taking my
               first steps in the world of work. While I may not have
